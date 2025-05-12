414ddb3ce187d289e2d9ec9f79fcc9e829a7a838
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-05-12 17:48:21 +0530

    added the question

--- Arrays and Strings/stringComparision.cpp
+++ Arrays and Strings/stringComparision.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 
+/*
+
+String Compression : Implement a method to perform basic string copression using the counts of repeated characters.
+For Example the string "aaaabbbccd" would become "a4b3c2d1".
+If the compressed string wouldnt become smaller than the original string, your method should return the original string.
+You can assume the string has only uppercase and lowercase letters(a - z);
+
+*/
+
+
 string stringCompress(string & s){
     int len = s.length();
     string temp = "";
